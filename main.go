965005b5b3bd492c5bd277e8459b0c416182f41a
yuanyu90221 <yuanyu90221@gmail.com>
2021-03-18 00:42:12 +0800

    change the parse order to dynamic change command condition

--- main.go
+++ main.go
@@ -5,11 +5,26 @@ import (
 	"log"
 )
 
+var name string
+
 func main() {
-	var name string
-	flag.StringVar(&name, "name", "Go", "info")
-	flag.StringVar(&name, "n", "Go", "info")
 	flag.Parse()
+	goCmd := flag.NewFlagSet("go", flag.ExitOnError)
+	goCmd.StringVar(&name, "name", "Go語言", "說明資訊")
+	phpCmd := flag.NewFlagSet("php", flag.ExitOnError)
+	phpCmd.StringVar(&name, "n", "PHP語言", "說明資訊")
+
+	args := flag.Args()
+	if len(args) <= 0 {
+		return
+	}
+
+	switch args[0] {
+	case "go":
+		_ = goCmd.Parse(args[1:])
+	case "php":
+		_ = phpCmd.Parse(args[1:])
+	}
 
 	log.Printf("name: %s", name)
 }
